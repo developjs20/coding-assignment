bf93f0b4a100892365f53d4744c6be8d7a260979
nina.vasetska <nina.vasetska@e-hps.com>
2024-05-07 22:46:37 +0200

    Add changes after Code review

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import { useEffect, useState } from 'react'
-import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from "react-router-dom"
+import { Routes, Route, createSearchParams, useSearchParams, useNavigate } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import 'reactjs-popup/dist/index.css'
 import { fetchMovies } from './data/moviesSlice'
-import { ENDPOINT_SEARCH, ENDPOINT_DISCOVER, ENDPOINT, API_KEY } from './constants'
+import {ENDPOINT_SEARCH, ENDPOINT_DISCOVER, getEndpointMovie} from './constants'
 import Header from './components/Header'
 import Movies from './components/Movies'
 import Starred from './components/Starred'
@@ -14,23 +14,23 @@ import './app.scss'
 const App = () => {
 
   const state = useSelector((state) => state)
-  const { movies } = state  
+  const { movies } = state
   const dispatch = useDispatch()
   const [searchParams, setSearchParams] = useSearchParams()
   const searchQuery = searchParams.get('search')
   const [videoKey, setVideoKey] = useState()
   const [isOpen, setOpen] = useState(false)
   const navigate = useNavigate()
-  
+
   const closeModal = () => setOpen(false)
-  
+
   const closeCard = () => {
 
   }
 
   const getSearchResults = (query) => {
     if (query !== '') {
-      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=`+query))
+      dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=${query}`))
       setSearchParams(createSearchParams({ search: query }))
     } else {
       dispatch(fetchMovies(ENDPOINT_DISCOVER))
@@ -45,25 +45,21 @@ const App = () => {
 
   const getMovies = () => {
     if (searchQuery) {
-        dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=`+searchQuery))
+        dispatch(fetchMovies(`${ENDPOINT_SEARCH}&query=${searchQuery}`))
     } else {
         dispatch(fetchMovies(ENDPOINT_DISCOVER))
     }
   }
 
-  const viewTrailer = (movie) => {
-    getMovie(movie.id)
-    if (!videoKey) setOpen(true)
+  const viewTrailer = async (movie) => {
+    await getMovie(movie.id)
     setOpen(true)
   }
 
   const getMovie = async (id) => {
-    const URL = `${ENDPOINT}/movie/${id}?api_key=${API_KEY}&append_to_response=videos`
-
     setVideoKey(null)
-    const videoData = await fetch(URL)
+    const videoData = await fetch(getEndpointMovie(id))
       .then((response) => response.json())
-
     if (videoData.videos && videoData.videos.results.length) {
       const trailer = videoData.videos.results.find(vid => vid.type === 'Trailer')
       setVideoKey(trailer ? trailer.key : videoData.videos.results[0].key)
@@ -84,7 +80,7 @@ const App = () => {
             videoKey={videoKey}
           />
         ) : (
-          <div style={{padding: "30px"}}><h6>no trailer available. Try another movie</h6></div>
+          isOpen ? (<div style={{padding: "30px"}}><h6>no trailer available. Try another movie</h6></div>) : null
         )}
 
         <Routes>
--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export const API_KEY = process.env.REACT_APP_API_KEY;
-console.log(process.env);
 export const ENDPOINT = 'https://api.themoviedb.org/3'
-export const ENDPOINT_DISCOVER = ENDPOINT+'/discover/movie?api_key='+API_KEY+'&sort_by=vote_count.desc'
-export const ENDPOINT_SEARCH = ENDPOINT+'/search/movie?api_key='+API_KEY
-export const ENDPOINT_MOVIE = ENDPOINT+'/movie/507086?api_key='+API_KEY+'&append_to_response=videos'
+export const ENDPOINT_DISCOVER = `${ENDPOINT}/discover/movie?api_key=${API_KEY}&sort_by=vote_count.desc`
+export const ENDPOINT_SEARCH = `${ENDPOINT}/search/movie?api_key=${API_KEY}`
+
+export const getEndpointMovie = (movieId) => `${ENDPOINT}/movie/${movieId}?api_key=${API_KEY}&append_to_response=videos`
