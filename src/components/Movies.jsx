5490a595348368c89f3c127f5211fa196eab8914
nina.vasetska <nina.vasetska@e-hps.com>
2024-05-08 15:16:20 +0200

    Make a grid display of movies list

--- src/components/Movies.jsx
+++ src/components/Movies.jsx
@@ -5,16 +5,18 @@ const Movies = ({ movies, viewTrailer, closeCard }) => {
 
     return (
         <div data-testid="movies">
-            {movies.movies.results?.map((movie) => {
-                return (
-                    <Movie 
-                        movie={movie} 
-                        key={movie.id}
-                        viewTrailer={viewTrailer}
-                        closeCard={closeCard}
-                    />
-                )
-            })}
+            <div class="row">
+                {movies.movies.results?.map((movie) => {
+                    return (
+                        <Movie
+                            movie={movie}
+                            key={movie.id}
+                            viewTrailer={viewTrailer}
+                            closeCard={closeCard}
+                        />
+                    )
+                })}
+            </div>
         </div>
     )
 }
